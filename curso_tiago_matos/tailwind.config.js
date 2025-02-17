c31d43566559747f524d37d9632756ff61850cab
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-02-17 16:39:27 -0300

    17/02 studies

--- curso_tiago_matos/tailwind.config.js
+++ curso_tiago_matos/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brand: {
+        100: '#c542f5',
+        200: '#a703ff',
+        300: '#4e1599'
+        }
+      },
+      padding:{
+        "teste": "10rem"
+      }
+    },
   },
   plugins: [],
 }
